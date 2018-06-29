6ccfb5b3714c4d79bcfdefc1a6684a383b401d41
dnguyen <nd.quang16@gmail.com>
2018-06-29 14:06:07 +0200

    init

---
+++ src/main/java/com/mrKhoai/webshop/beta/BetaApplication.java
@@ -0,0 +1,12 @@
+package com.mrKhoai.webshop.beta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mrKhoai/webshop/beta/BetaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mrKhoai.webshop.beta;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BetaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
