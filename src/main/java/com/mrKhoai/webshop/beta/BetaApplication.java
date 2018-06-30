6d57df086a9c267d4b663a96229a42d5b9740539
Trung <Trung.Dang@gt-arc.com>
2018-06-30 15:50:56 +0200

    test comit

--- src/main/java/com/mrKhoai/webshop/beta/BetaApplication.java
+++ src/main/java/com/mrKhoai/webshop/beta/BetaApplication.java
@@ -14,6 +14,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class BetaApplication {
 
 	public static void main(String[] args) {
+		//test commit bao 
 		SpringApplication.run(BetaApplication.class, args);
 	}
 }
