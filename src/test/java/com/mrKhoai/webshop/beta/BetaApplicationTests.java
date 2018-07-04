0ef9b1e9aae4818b701975b8c2c6c73c674e7e58
dnguyen6 <nd.quang16@gmail.com>
2018-07-04 02:51:57 +0200

    login.html

--- src/test/java/com/mrKhoai/webshop/beta/BetaApplicationTests.java
+++ src/test/java/com/mrKhoai/webshop/beta/BetaApplicationTests.java
@@ -2,15 +2,21 @@ package com.mrKhoai.webshop.beta;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ComponentScan(basePackages = "com.mrKhoai.webshop")
+@EntityScan(basePackages = "com.mrKhoai.webshop.objects")
+@EnableJpaRepositories(basePackages = "com.mrKhoai.webshop.repositories")
 public class BetaApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
