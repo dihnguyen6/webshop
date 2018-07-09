e0ceabb9515e8131b0d735961364f2e3fbb0723f
dnguyen <nd.quang16@gmail.com>
2018-07-09 23:53:24 +0200

    thymleaf fragments

--- src/main/java/com/mrKhoai/webshop/beta/MvcConfig.java
+++ src/main/java/com/mrKhoai/webshop/beta/MvcConfig.java
@@ -1,12 +1,16 @@
 package com.mrKhoai.webshop.beta;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
 
+    @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home-02");
@@ -14,5 +18,14 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addViewController("/admin").setViewName("admin/loginAdmin");
         registry.addViewController("/error").setViewName("error");
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(
+                "/basis")
+                .addResourceLocations(
+                        "classpath:/static/basis");
+    }
+
 
 }
