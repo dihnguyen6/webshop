aecb95031584765aa847e17fc5b6a89c8a286bbb
ducduy <ducduy.chu.de@gmail.com>
2018-07-14 02:10:39 +0200

    test deploy #3

--- src/main/java/com/mrKhoai/webshop/beta/WebConfig.java
+++ src/main/java/com/mrKhoai/webshop/beta/WebConfig.java
@@ -1,10 +1,15 @@
 package com.mrKhoai.webshop.beta;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(basePackages = "com.mrKhoai.webshop")
@@ -12,4 +17,23 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaRepositories(basePackages = "com.mrKhoai.webshop.repositories")
 @EnableTransactionManagement
 public class WebConfig {
+
+    @Bean
+    public DataSource dataSource(){
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setDataSourceClassName("org.postgresql.Driver");
+
+        hikariConfig.setMaximumPoolSize(5);
+        hikariConfig.setConnectionTestQuery("SELECT 1");
+        hikariConfig.setPoolName("springHikariCP");
+
+        hikariConfig.addDataSourceProperty("dataSource.cachePrepStmts", "true");
+        hikariConfig.addDataSourceProperty("dataSource.prepStmtCacheSize", "250");
+        hikariConfig.addDataSourceProperty("dataSource.prepStmtCacheSqlLimit", "2048");
+        hikariConfig.addDataSourceProperty("dataSource.useServerPrepStmts", "true");
+
+        HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+
+        return dataSource;
+    }
 }
