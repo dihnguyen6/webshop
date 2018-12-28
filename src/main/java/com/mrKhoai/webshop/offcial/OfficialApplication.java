2037fed6a4f4cefabfe348701e50d491165b04e6
dihnguyen <nd.quang16@gmail.com>
2018-12-29 00:49:25 +0100

    Version Official_v0.1

--- src/main/java/com/mrKhoai/webshop/offcial/OfficialApplication.java
+++ src/main/java/com/mrKhoai/webshop/offcial/OfficialApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BetaApplication {
+public class OfficialApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BetaApplication.class, args);
+        SpringApplication.run(OfficialApplication.class, args);
     }
 }
