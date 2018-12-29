c0d51ebe628dc6db0c06728b2f0f0ae97cf65554
dihnguyen <nd.quang16@gmail.com>
2018-12-29 15:57:30 +0100

    Split Controller

--- src/main/java/com/mrKhoai/webshop/controller/web/WebController.java
+++ src/main/java/com/mrKhoai/webshop/controller/web/WebController.java
@@ -8,9 +8,6 @@ import com.mrKhoai.webshop.controller.staff.StaffService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -138,52 +135,7 @@ public class WebController {
         return "web-dev/web-dev";
     }
 
-    @RequestMapping("/admin")
-    public String adminLogin(Model model, @RequestParam(value = "error", defaultValue = "none", required = false) String request) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        //Check if it isn't an authenticated user
-        if (authentication instanceof AnonymousAuthenticationToken || authentication == null || !authentication.isAuthenticated()) {
-            if (request.equals("none")) {
-                model.addAttribute("message", "Welcome back my admin!");
-                model.addAttribute("messColor", "green");
-            } else if (request.equals("loginError")) {
-                model.addAttribute("message", "Wrong username or password!");
-                model.addAttribute("messColor", "red");
-            }
-            return "admin/loginAdmin";
-        } else {
-            if(authentication.getName().equalsIgnoreCase(WebshopConst.ADMIN)) {
-                return "redirect:/admin/management";
-            } else {
-                return "admin/loginAdmin";
-            }
-        }
-    }
 
-    /*@RequestMapping("/admin/login")
-    public String admin_login() {
-        return "redirect:/admin/management";
-    }*/
-
-    @RequestMapping("/admin/management")
-    public String adminPage(Model model) {
-        return "admin/index";
-    }
-
-    @GetMapping("edit-carousel")
-    public String editCarousel() {
-        return "anonymous/edit-carousel";
-    }
-
-    @RequestMapping("/admin/product")
-    public String product(Model model) {
-        return "admin/product";
-    }
-
-    @RequestMapping("/admin/demo")
-    public String demo(Model model) {
-        return "admin/demo";
-    }
     /**
      * Get pramater "lang" from cookie in request header
      *
