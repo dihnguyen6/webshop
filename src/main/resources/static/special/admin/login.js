91ae21706297888ef74b5cd3028cb3161b0dd259
dihnguyen <nd.quang16@gmail.com>
2019-02-26 10:30:25 +0100

    Login to Admin - Done

--- src/main/resources/static/special/admin/login.js
+++ src/main/resources/static/special/admin/login.js
@@ -1,3 +1,19 @@
+function createAlertMess() {
+    jQuery('<div/>', {
+        id: 'alertMess',
+        class: 'alert alert-warning alert-dismissible fadeIn show',
+        role: 'alert'
+    }).appendTo('body')
+
+    jQuery('<h1/>', {
+        text: 'Notification'
+    }).appendTo('#alertMess');
+
+    jQuery('<p/>', {
+        id: 'mess'
+    }).appendTo('#alertMess');
+}
+
 window.setTimeout(function () {
     $('#alertMess').fadeTo(1000, -100).slideUp(500, function () {
         $(this).remove();
